2e98ecf98b7f6090f25d042e39001dc42a2a146b
ysur67 <rus.muratow@gmail.com>
2021-09-10 12:33:57 +0300

    feat: control device fragment

--- app/src/main/java/com/example/omron/di/component/AppComponent.kt
+++ app/src/main/java/com/example/omron/di/component/AppComponent.kt
@@ -4,6 +4,7 @@ import com.example.omron.di.module.AppModule
 import com.example.omron.di.module.RepositoryModule
 import com.example.omron.di.module.ViewModelModule
 import com.example.omron.presentation.activity.MainActivity
+import com.example.omron.presentation.fragment.ControlDeviceFragment
 import com.example.omron.presentation.fragment.ScannedDevicesFragment
 import dagger.Component
 import javax.inject.Singleton
@@ -14,4 +15,5 @@ interface AppComponent {
     fun inject(mainActivity: MainActivity)
 
     fun inject(scannedDevicesFragment: ScannedDevicesFragment)
+    fun inject(controlDeviceFragment: ControlDeviceFragment)
 }
---
+++ app/src/main/java/com/example/omron/presentation/fragment/ControlDeviceFragment.kt
@@ -0,0 +1,54 @@
+package com.example.omron.presentation.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.example.omron.OmronApp
+import com.example.omron.R
+import com.example.omron.databinding.FragmentControlDeviceBinding
+import com.example.omron.di.ViewModelFactory
+import com.example.omron.domain.implementation.ConnectionViewModel
+import javax.inject.Inject
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ControlDeviceFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ControlDeviceFragment : Fragment() {
+    private var _binding: FragmentControlDeviceBinding? = null
+    private val binding get() = _binding!!
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+    private val connectionViewModel: ConnectionViewModel by activityViewModels { viewModelFactory }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (activity?.application as OmronApp).appComponent.inject(this)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentControlDeviceBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment.
+         *
+         * @return A new instance of fragment ControlDeviceFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance() = ControlDeviceFragment()
+    }
+}
