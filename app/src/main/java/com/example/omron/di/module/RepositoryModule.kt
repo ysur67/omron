bc86cdc6a1d1feb404ad5c0a96c922271d976fb5
ysur67 <rus.muratow@gmail.com>
2021-09-10 10:27:43 +0300

    feat: add connection repo to dagger

--- app/src/main/java/com/example/omron/di/module/RepositoryModule.kt
+++ app/src/main/java/com/example/omron/di/module/RepositoryModule.kt
@@ -1,8 +1,10 @@
 package com.example.omron.di.module
 
 import android.content.Context
+import com.example.omron.data.repository.ConnectionRepository
 import com.example.omron.data.repository.implementation.OmronScanRepository
 import com.example.omron.data.repository.ScanRepository
+import com.example.omron.data.repository.implementation.OmronConnectionRepository
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -14,5 +16,11 @@ class RepositoryModule {
     @Singleton
     fun provideScanRepository(context: Context) : ScanRepository {
         return OmronScanRepository(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideConnectionRepository(context: Context) : ConnectionRepository {
+        return OmronConnectionRepository(context)
     }
 }
